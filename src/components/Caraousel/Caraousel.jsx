15556837c2aa14511eaf980fa19c3fba5b4fd4b7
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-03-15 13:05:48 +0530

    feat - added caraousel for landing-page

--- src/components/Caraousel/Caraousel.jsx
+++ src/components/Caraousel/Caraousel.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { prototype } from "jsonwebtoken/lib/JsonWebTokenError";
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-function Caraousel() {
-  return <div>Caraousel</div>;
-}
+import "./caraousel.scss";
+
+const Caraousel = ({ data = [] }) => {
+  const [numOfSlides, setNumOfSlides] = useState(0);
+
+  const movePrev = () => {
+    setNumOfSlides((slideNum) => (slideNum - 1) % data?.length);
+  };
+
+  const moveNext = () => {
+    setNumOfSlides((slideNum) => (slideNum + 1) % data?.length);
+  };
+
+  const moveDot = (index) => {
+    setNumOfSlides(index);
+  };
+
+  useEffect(() => {
+    let intervalID = setInterval(() => moveNext(), 4000);
+
+    return () => {
+      clearImmediate(intervalID);
+    };
+  }, []);
+
+  return (
+    <section className="carousel">
+      {data?.map((item, index) => (
+        <article
+          className={`carousel__image-container ${
+            numOfSlides === index && "animate"
+          }`}
+          key={item.id}
+        >
+          <img
+            src={item.imgSrc}
+            alt={item.altText}
+            className="carousel__image-container__image"
+          />
+          <h2 className="carousel__description">{item.imageDescription}</h2>
+        </article>
+      ))}
+
+      <div className="carousel__button-container">
+        <button
+          className="carousel__button-container-btn"
+          onClick={() => movePrev()}
+        >
+          <i className="fa-solid fa-circle-arrow-left" />
+        </button>
+        <button
+          className="carousel__button-container-btn"
+          onClick={() => moveNext()}
+        >
+          <i className="fa-solid fa-circle-arrow-right" />
+        </button>
+      </div>
+
+      <div className="carousel__dots-container">
+        {Array(data ? data.length : 0)
+          .fill(1)
+          .map((item, index) => (
+            <div
+              onClick={() => moveDot(index)}
+              className={numOfSlides === index ? "dot active" : "dot"}
+            ></div>
+          ))}
+      </div>
+    </section>
+  );
+};
+
+Caraousel.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string.isRequired,
+      imageDescription: PropTypes.string.isRequired,
+      altText: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default Caraousel;
