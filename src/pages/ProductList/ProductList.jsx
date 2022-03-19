7c9d8da95dd6ff9e2140c0119d3cada11e89a04b
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-03-19 23:59:37 +0530

    fix - image name

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -13,7 +13,7 @@ import {
 import { ErrorPage } from "../ErrorPage";
 
 // Styles, Images
-import notFound from "../../assets/productNotFound.jpg";
+import notFound from "../../assets/productnotfound.jpg";
 import "./product-list.scss";
 
 function ProductList() {
