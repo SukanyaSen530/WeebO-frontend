81e63fa42fdcf03b243c21f6d5f450d7fde84052
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-04-06 20:25:49 +0530

    added react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.scss";
 import { Navbar, Footer } from "./components";
 
 import AllRoutes from "./Routes/AllRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <section className="main-section">
+        <ToastContainer />
         <AllRoutes />
       </section>
       <Footer />
