54bdfae72ad16c7d24f6fafbf9eadeb3d7fc0fdf
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-04-07 11:48:51 +0530

    feat - added user details navigation links

--- src/pages/UserDetails/UserDetails.jsx
+++ src/pages/UserDetails/UserDetails.jsx
@@ -1,13 +1,54 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
+import "./user-details.scss";
+
+import { MdLocationOn } from "react-icons/md";
+import { FaBoxOpen, FaUserCog } from "react-icons/fa";
+
+const menuOptions = [
+  {
+    id: "u1",
+    path: "/user",
+    icon: <FaUserCog />,
+    name: "Profile",
+    details: "Edit Details",
+  },
+  {
+    id: "u2",
+    path: "/user/address",
+    icon: <MdLocationOn />,
+    name: "Addresses",
+    details: "All Addresses",
+  },
+  {
+    id: "u3",
+    path: "/user/orders",
+    icon: <FaBoxOpen />,
+    name: "Orders",
+    details: "Your Orders",
+  },
+];
+
 const UserDetails = () => {
   return (
-    <section>
-      <Link to="/user">Profile</Link>
-      <Link to="/user/address">Address</Link>
-      <Link to="/user/orders">Orders</Link>
-      <div>
+    <section className="user-details-section flex flex-col flex-center">
+      <div className="flex user-details-section__menu">
+        {menuOptions.map((item) => (
+          <Link
+            className="user-details-section__links"
+            key={item.id}
+            to={item.path}
+          >
+            <p className="user-details-section__links__icon">{item.icon}</p>
+            <div className="flex flex-col user-details-section__links__items">
+              <span>{item.name}</span>
+              <span>{item.details}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="user-details-section__outlet">
         <Outlet />
       </div>
     </section>
